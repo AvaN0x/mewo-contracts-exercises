7cf0169165907d26e972f7e3608ae8369b2bacfb
Clement RICATTE <clement.ricatte57@gmail.com>
2023-05-22 16:24:52 +0200

    exercices

--- src/010/Crowdsale.sol
+++ src/010/Crowdsale.sol
@@ -2,16 +2,25 @@
 pragma solidity ^0.8.20;
 
 import { IERC20 } from "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import { Ownable } from "openzeppelin-contracts/contracts/access/Ownable.sol";
 
 // Create a contract that allow users to buy an ERC20 (Mewo) tokens with ethers.
 // The Crowdsale contract will automatically receive 1,000,000,000 tokens (e.g mewo.balanceOf(address(Crowdsale)) == 1e9).
 // Token will be sold with the rate: 1 ether = 10000 tokens.
 // As always, add withdraw functiononly callable the owner of the contract but use the Ownable contract from the OpenZeppelin library.
-contract Crowdsale {
+contract Crowdsale is Ownable {
   uint256 public constant RATE = 10000;
   IERC20 token;
 
-  constructor(IERC20 _token) { }
+  constructor(IERC20 _token) {
+    token = _token;
+  }
 
-  function buy() public payable { }
+  function buy() public payable {
+    token.transfer(msg.sender, msg.value * RATE);
+  }
+
+  function withdraw() public onlyOwner {
+    payable(owner()).transfer(payable(address(this)).balance);
+  }
 }
--- src/011/MyNFT.sol
+++ src/011/MyNFT.sol
@@ -9,12 +9,36 @@ import { ERC721 } from "openzeppelin-contracts/contracts/token/ERC721/ERC721.sol
 contract MyNFT is ERC721 {
   uint256 public constant EARLY = 0.05 ether;
   uint256 public constant NORMAL = 0.1 ether;
+  uint256 public constant EARLY_SUPPLIES = 1_000;
+  uint256 public constant MAXIMUM_SUPPLIES = 10_000;
+  uint256 public totalSupplies;
 
   error InsufficientValue(uint256 expected, uint256 actual);
 
   constructor() ERC721("MyNFT", "MNFT") { }
 
-  function price(uint256 quantity) internal view returns (uint256) { }
+  function min(uint256 a, uint256 b) internal pure returns (uint256) {
+    return a < b ? a : b;
+  }
 
-  function purchase(uint256 quantity) external payable { }
+  function price(uint256 quantity) internal view returns (uint256) {
+    uint256 earlyQuantity = totalSupplies > EARLY_SUPPLIES ? 0 : min(quantity, EARLY_SUPPLIES);
+    uint256 normalQuantity = quantity - earlyQuantity;
+
+    return earlyQuantity * EARLY + normalQuantity * NORMAL;
+  }
+
+  function purchase(uint256 quantity) external payable {
+    require(quantity > 0);
+    require(totalSupplies + quantity <= MAXIMUM_SUPPLIES);
+    uint256 totalPrice = price(quantity);
+    if (msg.value < totalPrice) {
+      revert InsufficientValue(totalPrice, msg.value);
+    }
+
+    for (uint256 i = totalSupplies; i < totalSupplies + quantity; i++) {
+      _safeMint(msg.sender, i);
+    }
+    totalSupplies += quantity;
+  }
 }
